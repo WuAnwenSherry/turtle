3117a9856a2e20e3e9b386de75392530ccb3e3a1
Anwen <46390115+WuAnwenSherry@users.noreply.github.com>
2022-07-02 22:04:34 +0800

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,37 @@
+from turtle import *
+from random import *
+from math import *
+
+def tree(n, l):
+    pd()
+    t = cos(radians(heading() + 45)) / 8 + 0.25
+    pencolor(t, t* 1.25, t *0.55) #85,107,47
+    pensize(n / 2)
+    forward(l)
+    if (n > 0):
+        b = random() * 15 + 10
+        c = random() * 15 + 10
+        d = l * (random() * 0.35 + 0.6)
+        right(b)
+        tree(n - 1, d)
+        left(b + c)
+        tree(n - 1, d)
+        right(c)
+    else:
+        right(90)
+        n = cos(radians(heading() - 45)) / 4 + 0.5
+        pencolor(n, n * 1.25, n * 0.55)
+        circle(2)
+        left(90)
+    pu()
+    backward(l)
+
+bgcolor(0.5, 0.5, 0.5)
+ht()
+speed(0)
+tracer(0, 0)
+left(90)
+pu()
+backward(300)
+tree(12, 80)
+done()
